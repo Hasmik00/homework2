9fc3750d53e885328240d8cd22647ecce8bed8c4
Hasmik00 <Hasmik_ananyan@edu.aua.am>
2018-09-10 23:58:35 +0400

    ADD: js

---
+++ js/homework.js
@@ -0,0 +1,16 @@
+const a = 2;
+const b = 10;
+const c = 7;
+const joe = "Poghosyan"; 
+
+console.log(b - a);
+console.log(b + c);
+console.log(c * a);
+console.log(c / a);
+
+if((b + c) > (c + a)){
+	console.log("Hello World");
+} else{
+	console.log("Hi");
+}
+
